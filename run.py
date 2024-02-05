0b6af8660c9370418e0614c552626de3d47cccb5
brencoding1 <brencoding1@gmail.com>
2024-02-05 04:45:27 +0000

    Add first minimal strings with basic structure information of game including rules and instructions

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Battleships code file.
+Player info: - The maximum size of the grid is 10 rows and 10 columns.
+There will always be an equal number of rows and columns on the board.
+The size selected by you will multiply by itself.
+Letters are used to represent columns.
+Numbers are used to represent rows.
+You'll predict rows first, then columns, each time.
+The battleships on the board will be concealed.
+A "O" will appear if you strike a ship.
+Missing a ship will result in a "X" being placed next to it.
+"""
